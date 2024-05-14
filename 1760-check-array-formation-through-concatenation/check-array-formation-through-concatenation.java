5094be93937941122a4bd7876ec74680fcae4612
mahakxd <160941408+mahakxd@users.noreply.github.com>
2024-05-14 23:50:25 +0530

    Time: 1 ms (88.89%) | Memory: 41.6 MB (83.33%) - LeetSync

--- 1760-check-array-formation-through-concatenation/check-array-formation-through-concatenation.java
+++ 1760-check-array-formation-through-concatenation/check-array-formation-through-concatenation.java
@@ -1,38 +1,19 @@
 class Solution {
-    public boolean canFormArray(int[] A, int[][] P) {
-        Map<Integer, int[]> map = new HashMap<>(); //number -> left & right numbers
-        
-        for(int i = 0; i < P.length; ++i) {
-            for(int j = 0; j < P[i].length;++ j) {
-                
-                int left = -1, right = -1;
-                if(j > 0) {
-                    left = P[i][j - 1];
-                }
-                if(j < P[i].length - 1) {
-                    right = P[i][j + 1];
-                }
-                
-                map.put(P[i][j], new int[] {left, right});
-            }
+    public boolean canFormArray(int[] arr, int[][] pieces) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<pieces.length; i++){
+            map.put(pieces[i][0], i);
         }
-        
-        for(int i = 0; i < A.length; ++i) {
-            int left = -1, right = -1;
-            if(i > 0) {
-                left = A[i - 1];
+        int i=0;
+        while(i< arr.length){
+            if(map.containsKey(arr[i])){
+                int[] piece = pieces[map.get(arr[i])];
+                for(int p: piece){
+                    if(arr[i++]!=p) return false;
+                }
             }
-            if(i < A.length - 1) {
-                right = A[i + 1];
-            }
-            
-            if(!map.containsKey(A[i])) return false;
-            
-            var have = map.get(A[i]);
-            if((have[0] > -1 && have[0] != left) || (have[1] > -1 && have[1] != right)) return false;
+            else return false;
         }
-        
         return true;
-        
     }
 }
