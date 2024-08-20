228772a1be6fd6a5f8c2e46cec21619d8b93b843
MAHAK AGARWAL <mahaka365@gmail.com>
2024-08-20 19:12:10 +0530

    Time: 1 ms (64.38%) | Memory: 41.1 MB (29.17%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -1,25 +1,16 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
+        List<Integer> result = new ArrayList<>(Collections.nCopies(rowIndex + 1, 0));
+        result.set(0, 1);
 
-        List<List<Integer>> TriangleArr = new ArrayList<>();
+        for (int row = 1; row <= rowIndex; ++row) {
+            result.set(row, 1);
 
-        for (int i = 0; i <= rowIndex; i++) {
-            List<Integer> Row = new ArrayList<>();
-
-            for (int j = 0; j <= i; j++) {
-                if (j == 0 || j==i) {
-                    Row.add(1);
-                } else {
-                    int sum = TriangleArr.get(i - 1).get(j - 1) + TriangleArr.get(i - 1).get(j);
-                    Row.add(sum);
-                }
-
+            for (int index = row - 1; index > 0; --index) {
+                result.set(index, result.get(index) + result.get(index - 1));
             }
-            TriangleArr.add(Row);
-
         }
 
-        return TriangleArr.get(rowIndex);
-
+        return result;
     }
 }
