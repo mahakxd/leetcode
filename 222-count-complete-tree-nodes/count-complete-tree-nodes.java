b198b2cd10944573a2ac7102c360f4fefaf28a4b
MAHAK AGARWAL <mahaka365@gmail.com>
2024-08-25 23:47:23 +0530

    Time: 0 ms (100.00%) | Memory: 47.9 MB (16.87%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -1,7 +1,10 @@
 class Solution {
     public int countNodes(TreeNode root) {
-        if(root == null) return 0 ;
-        
-        return (countNodes(root.left) + countNodes(root.right))+1 ;
+        if(root == null) return 0;
+        return helper(root) - 1;
+    }
+    private int helper(TreeNode root) {
+        if(root == null) return 1;
+        return helper(root.right) + helper(root.left);
     }
 }
