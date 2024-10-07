491d3a536c9a759ee7e7465be5f8b5a28e9318e3
MAHAK AGARWAL <mahaka365@gmail.com>
2024-10-07 23:54:56 +0530

    Time: 5 ms (65.18%) | Memory: 45.4 MB (8.15%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,7 +1,8 @@
 class Solution {
   public int minDistance(String word1, String word2) {
     final int m = word1.length();//first word length
-    final int n = word2.length();
+    final int n = word2.length();///second word length
+    // dp[i][j] := min # of operations to convert word1[0..i) to word2[0..j)
     int[][] dp = new int[m + 1][n + 1];
 
     for (int i = 1; i <= m; ++i)
@@ -15,7 +16,7 @@ class Solution {
         if (word1.charAt(i - 1) == word2.charAt(j - 1))//same characters
           dp[i][j] = dp[i - 1][j - 1];//no operation
         else
-          dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;                     
+          dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;                      //replace               //delete        //insert
 
     return dp[m][n];
   }
